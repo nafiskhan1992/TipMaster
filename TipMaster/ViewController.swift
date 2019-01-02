1053366628c98e6e7d6cee8c25f32176826dd896
Nafis <nafisprottoy@nafiss-macbook-pro.local>
2019-01-01 19:57:19 -0500

    Initial Commit

---
+++ TipMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipMaster
+//
+//  Created by Nafis Prottoy on 1/1/19.
+//  Copyright © 2019 Nafis Prottoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
